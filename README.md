e5b7a1c39cf5883a8dfb3aa035ba861a0dd3e65d
Imed <imedtayeb9@gmail.com>
2024-01-11 19:52:32 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Getting Started
+
+Clone this project from https://github.com/abdallahsekrafi/React_Router
+
+# Instal NPM
+
+npm install
+
+# Instal React Router Dom
+
+npm install react-router-dom
+
+# Instal used components
+
+   - React Bootstrap:
+
+npm install react-bootstrap bootstrap
+
+   - Material UI:
+
+npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+
+# Runs the app
+
+npm start
+
+# Enjoy
+
+Open (http://localhost:3000) to view it in your browser.
+
+# follow me
+
+Sekrafi Abdallah (in every social media)
---
+++ src/components/Movies.js
@@ -0,0 +1,23 @@
+export const DefautlMovies = [
+    {
+        title: "Apocalypto",
+        description: "Un jeune homme est embarqué dans un voyage périlleux vers un monde régi par la peur et l'oppression",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BMzhmNGMzMDMtZDM0Yi00MmVmLWExYjAtZDhjZjcxZDM0MzJhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_FMjpg_UX1200_.jpg",
+        trailerlink:"https://www.youtube.com/embed/ngWBddVNVZs?si=WZ3gondwsEE2jNnu",
+        rating: "5"
+    },
+    {
+        title: "Jumanji: Welocome to the jungle",
+        description: "Quatre adolescents sont aspirés dans un jeu vidéo magique, et la seule façon de s'échapper est de travailler ensemble",
+        posterURL: "https://m.media-amazon.com/images/M/MV5BYzBmMTBmOWQtMTg3Zi00Mzk4LTk3OTMtYmU2NWMwYmJkNTU1XkEyXkFqcGdeQXVyMTEyOTQ1OTk4._V1_FMjpg_UX1200_.jpg",
+        trailerlink:"https://www.youtube.com/embed/2QKg5SZ_35I?si=nZO7F9uKYoVPQ9Nc",
+        rating: "3"
+    },
+    {
+        title: "Raya and the Last Dragon",
+        description: "Au royaume de Lumandra sur une Terre réinventée, une guerrière nommée Raya est déterminée à trouver le dernier dragon.",
+        posterURL: "https://upload.wikimedia.org/wikipedia/en/e/ea/Raya_and_the_Last_Dragon.png",
+        trailerlink:"https://www.youtube.com/embed/9BPMTr-NS9s?si=AitmPzbue_Wd7Q5Z",
+        rating: "4.5"
+    },
+];
